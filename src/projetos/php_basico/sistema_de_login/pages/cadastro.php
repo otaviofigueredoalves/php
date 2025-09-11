ac7275020956f5699024bad86420f0563b85e56d
otaviofgalves <117609558+otaviofigueredoalves@users.noreply.github.com>
2025-09-11 13:45:32 -0300

    feat: projeto sistema de login

--- src/projetos/php_basico/sistema_de_login/pages/cadastro.php
+++ src/projetos/php_basico/sistema_de_login/pages/cadastro.php
@@ -1,1 +1,19 @@
+<?php
+include __DIR__ . '/back_login.php'; 
+?>
 <h1>CADASTRO</h1>
+<form action="" method="POST">
+   <div>
+       <label for="user_name">Nome</label>
+       <input type="text" name="user_name" id="user_name">
+   </div>
+   <div>
+       <label for="user_email">Email</label>
+       <input type="text" name="user_email" id="user_email">
+   </div>
+   <div>
+       <label for="user_password">Senha</label>
+       <input type="text" name="user_password" id="user_password">
+   </div>
+   <input type="submit" value="Enviar">
+</form>
