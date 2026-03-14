d7032948b6e34fec4310ed6a37cf56f57b2ce1a1
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-03-14 02:10:15 -0300

    feat: chamando as views através dos controllers e melhorando um pouco o controller

--- src/mvc/app/controllers/HomeController.php
+++ src/mvc/app/controllers/HomeController.php
@@ -4,7 +4,7 @@ class HomeController
 {
     public function index()
     {
-        echo "Hello from index HomeController! ";
+        require_once __DIR__ . '/../views/home/index.php';
     }
     public function falarNome($nome)
     {
@@ -12,4 +12,22 @@ class HomeController
     }
 }
 
-// Isso viola o princípio de Separação de Preocupações (SoC). Os controladores nunca devem trabalhar com dados diretamente pra view
+/**
+ * Por que o require_once direto no Controller é ruim? 
+ * 
+ * Caminhos repetitivos: Você vai escrever __DIR__ . '/../views/' em todos os métodos. Se mudar a pasta, boa sorte alterando 50 arquivos.
+ * 
+ * Escopo de variáveis: Se você usar extract(), as variáveis ficam limpas e disponíveis apenas naquela View.
+ * 
+ * Testabilidade: Fica impossível testar o Controller isoladamente se ele estiver dando require e echo o tempo todo.
+ */
+/*
+
+O Paradigma em resumo:
+
+    Model: Busca o dado (ex: Select * from users).
+
+    Controller: Recebe o dado do Model e diz: "View, tome esse array aqui e se vire para mostrar pro usuário".
+
+    View: É apenas um arquivo com HTML e uns pequenos <?php echo $variavel; ?> nos lugares certos.
+/*
