0500b4b920f25c9ae8fd4068c3136134ef0595b5
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-03-23 00:16:51 -0300

    feat: refatoração para o padrão singleton

--- src/mvc/app/Controllers/HomeController.php
+++ src/mvc/app/Controllers/HomeController.php
@@ -3,17 +3,22 @@ namespace App\Controllers;
 use App\Core\Controller;
 use App\Model\Usuario;
 use App\Core\Database;
+use Exception;
+
 class HomeController extends Controller
 {
   
     public function index()
     {
-        $user = new Usuario();
-        $user_data = $user->getDataUser();
-        $database = new Database();
-        // $database->connect();
-        $consulta = $database->query("SELECT 1+1 AS teste");
-        dd($consulta->fetch());
+        try{
+            $user = new Usuario();
+            $user_data = $user->getDataUser();
+            $consulta = $user->testDb();
+            dd($consulta);
+        } catch (\PDOException $e){
+            throw new Exception("algum erro aconteceu no HomeController" . $e->getMessage());
+        }
+
         $this->view('home/index',$user_data); // agora só passar a rota pro controller montar/renderizar a view
     } 
 
